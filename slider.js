725c9c4d00c1fbe869e3457428870fe31a98f947
doggang <69455222+doggang@users.noreply.github.com>
2021-09-07 22:58:00 +0900

    aside바 완성, 슬라이드쇼 완성, nav바 그림자 생성

---
+++ slider.js
@@ -0,0 +1,28 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// 다음, 이전 제어
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// 사진 제어
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
